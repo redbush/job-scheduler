9dd8983970b9c5fa103e1d8c2acd1a4fb90f8161
Brian Rosedale <brian.j.rosedale@gmail.com>
2019-09-04 15:09:47 -0700

    Add javadocs for event producer

--- job-scheduler-app/src/main/java/brian/scheduler/app/producer/JobEventProducer.java
+++ job-scheduler-app/src/main/java/brian/scheduler/app/producer/JobEventProducer.java
@@ -4,8 +4,19 @@ import java.util.List;
 
 import brian.scheduler.comm.event.JobEvent;
 
+/**
+ * Producer to send a JobEvent to a destination
+ * 
+ * @param <T> the job event type
+ */
 public interface JobEventProducer<T extends JobEvent> {
 
+	/**
+	 * Sends the JobEvents to a destination
+	 *  
+	 * @param jobEvents the list of events to send
+	 * @throws Exception thrown if an error occurs sending the events
+	 */
 	void send(final List<T> jobEvents) throws Exception;
 	
 }
--- job-scheduler-app/src/main/java/brian/scheduler/app/producer/RedisJobEventProducer.java
+++ job-scheduler-app/src/main/java/brian/scheduler/app/producer/RedisJobEventProducer.java
@@ -5,23 +5,33 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Component;
 
 import brian.scheduler.comm.event.ExecuteJobEvent;
 
+/**
+ * Event producer which sends {@link ExecuteJobEvent}s to a Redis Stream. The
+ * Redis Stream is consumed by the job-agent services which executes the linux
+ * commands on the host they are running on.
+ */
 @Component
 public class RedisJobEventProducer implements JobEventProducer<ExecuteJobEvent> {
 
+	private static final String REDIS_JOB_STREAM_NAME = "JOB_STREAM";
 	private final RedisTemplate<String, ExecuteJobEvent> redisTemplate;
 	
-	public RedisJobEventProducer(
-			final RedisTemplate<String, ExecuteJobEvent> redisTemplateIn,
-			final JmsTemplate jmsTemplateIn) {
+	/**
+	 * @param redisTemplateIn the Redis template to write the events to a stream 
+	 */
+	public RedisJobEventProducer(final RedisTemplate<String, ExecuteJobEvent> redisTemplateIn) {
 		
 		redisTemplate = redisTemplateIn;
 	}
 	
+	/**
+	 * Writes each of the job events to a Redis Stream. The job-agent services
+	 * consume events added to the stream and execute the commands on that host.
+	 */
 	@Override
 	public void send(final List<ExecuteJobEvent> jobEvents) throws Exception {
 		
@@ -29,7 +39,7 @@ public class RedisJobEventProducer implements JobEventProducer<ExecuteJobEvent> 
 		for(ExecuteJobEvent jobEvent : jobEvents) {
 			jobsForStream.put(jobEvent.getId(), jobEvent);
 		}
-		redisTemplate.opsForStream().add("JOB_STREAM", jobsForStream);
+		redisTemplate.opsForStream().add(REDIS_JOB_STREAM_NAME, jobsForStream);
 	}
 	
 }
